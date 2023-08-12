410b92485020ce063221664068e4f1dc29fabf7f
Michael <michaellngriffin@gmail.com>
2023-08-12 09:15:40 -0400

    getShowsByTerm and displayShows working, still need episodes

--- tvmaze.js
+++ tvmaze.js
@@ -12,31 +12,48 @@ const BASE_URL = 'http://api.tvmaze.com';
  *    (if no image URL given by API, put in a default image URL)
  */
 
+
+//TODO: get show.id passed to handleEpisodes function properly
+//build displayEpisodes
 async function getShowsByTerm(searchTerm) {
   // ADD: Remove placeholder & make request to TVMaze search shows API.
-  const params = new URLSearchParams({q : searchTerm});
+  const params = new URLSearchParams({ q: searchTerm });
   const response = await fetch(`${BASE_URL}/search/shows?${params}`);
   const result = await response.json();
-  console.log(result);
+  //console.log(result);
 
-  return [
-    {
-      id: 1767,
-      name: "The Bletchley Circle",
-      summary:
-        `<p><b>The Bletchley Circle</b> follows the journey of four ordinary 
-           women with extraordinary skills that helped to end World War II.</p>
-         <p>Set in 1952, Susan, Millie, Lucy and Jean have returned to their 
-           normal lives, modestly setting aside the part they played in 
-           producing crucial intelligence, which helped the Allies to victory 
-           and shortened the war. When Susan discovers a hidden code behind an
-           unsolved murder she is met by skepticism from the police. She 
-           quickly realises she can only begin to crack the murders and bring
-           the culprit to justice with her former friends.</p>`,
-      image:
-          "http://static.tvmaze.com/uploads/images/medium_portrait/147/369403.jpg"
-    }
-  ]
+  let formattedShows = result.map(item => {
+    let show = item.show;
+    let { id, name, summary, image } = show;
+    let formattedShow = {
+      id,
+      name,
+      summary,
+      image
+    };
+    return formattedShow;
+  });
+  //console.log(formatted);
+  return formattedShows;
+
+  /*   [
+      {
+        id: 1767,
+        name: "The Bletchley Circle",
+        summary:
+          `<p><b>The Bletchley Circle</b> follows the journey of four ordinary
+             women with extraordinary skills that helped to end World War II.</p>
+           <p>Set in 1952, Susan, Millie, Lucy and Jean have returned to their
+             normal lives, modestly setting aside the part they played in
+             producing crucial intelligence, which helped the Allies to victory
+             and shortened the war. When Susan discovers a hidden code behind an
+             unsolved murder she is met by skepticism from the police. She
+             quickly realises she can only begin to crack the murders and bring
+             the culprit to justice with her former friends.</p>`,
+        image:
+            "http://static.tvmaze.com/uploads/images/medium_portrait/147/369403.jpg"
+      }
+    ] */
 }
 
 
@@ -47,14 +64,19 @@ async function getShowsByTerm(searchTerm) {
 
 function displayShows(shows) {
   $showsList.empty();
+  const missingImage = 'https://tinyurl.com/tv-missing';
 
+
+  //May need to modify below to add in a button
   for (const show of shows) {
+    let displayedImage = (show.image) ? show.image.medium : missingImage;
+
     const $show = $(`
         <div data-show-id="${show.id}" class="Show col-md-12 col-lg-6 mb-4">
          <div class="media">
-           <img 
-              src="http://static.tvmaze.com/uploads/images/medium_portrait/160/401704.jpg" 
-              alt="Bletchly Circle San Francisco" 
+           <img
+              src=${displayedImage}
+              alt="Bletchly Circle San Francisco"
               class="w-25 me-3">
            <div class="media-body">
              <h5 class="text-primary">${show.name}</h5>
@@ -66,9 +88,14 @@ function displayShows(shows) {
          </div>
        </div>
       `);
-
+    //Could add event listeners to each button, do this within loop
+    $('.Show-getEpisodes').on('click', () => {
+      getEpisodesOfShow(show.id);
+    });
     $showsList.append($show);
   }
+
+  //"http://static.tvmaze.com/uploads/images/medium_portrait/160/401704.jpg"
 }
 
 
@@ -84,17 +111,27 @@ async function searchShowsAndDisplay() {
   displayShows(shows);
 }
 
-$searchForm.on("submit", async function handleSearchForm (evt) {
+$searchForm.on("submit", async function handleSearchForm(evt) {
   evt.preventDefault();
   await searchShowsAndDisplay();
 });
+
+
 
 
 /** Given a show ID, get from API and return (promise) array of episodes:
  *      { id, name, season, number }
  */
 
-// async function getEpisodesOfShow(id) { }
+async function getEpisodesOfShow(id) {
+
+  //make fetch call to
+  //http://api.tvmaze.com/shows/[showid]/episodes
+  let episodesURL = `${BASE_URL}/shows/${id}/episodes`;
+  const response = await fetch(episodesURL);
+  const result = await response.json();
+  console.log(result);
+}
 
 /** Write a clear docstring for this function... */
 
